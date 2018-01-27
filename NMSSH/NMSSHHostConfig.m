15e1cd7068f36c5a013d5f685c979150c8f1b7ff
Martin Imobersteg <martin.imobersteg@gmail.com>
2018-01-27 13:49:22 +0100

    instancetype

--- NMSSH/NMSSHHostConfig.m
+++ NMSSH/NMSSHHostConfig.m
@@ -2,7 +2,7 @@
 
 @implementation NMSSHHostConfig
 
-- (id)init {
+- (instancetype)init {
     if ((self = [super init])) {
         [self setHostPatterns:@[ ]];
         [self setIdentityFiles:@[ ]];
